1c026a91e5a30fe5340288a290c0f23f8e623e17
yoshi1220 <19341602+yoshi1220@users.noreply.github.com>
2024-08-27 22:58:02 +0900

    プロジェクト ファイルを追加します。

---
+++ TimeTracker.API/Controllers/TimeEntryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Shared.Entities;
+
+namespace TimeTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeEntryController : ControllerBase
+    {
+        private static List<TimeEntry> _timeEntries = new List<TimeEntry>
+        {
+            new TimeEntry
+            {
+                Id = 1,
+                Project = "Time Tracker App",
+                End  =DateTime.Now.AddHours(1)
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<TimeEntry>> GetAllEntries()
+        {
+            return Ok(_timeEntries);
+        }
+    }
+}
